dc2fc7f5a704a4684cc0ae2353ebba7bbe4cf398
Assem Khashaba <assemkhashaba0@gmail.com>
2024-10-06 00:55:30 +0300

    Add project files.

---
+++ Hospital/Controllers/DoctorsController.cs
@@ -0,0 +1,49 @@
+﻿using Hospital.Data;
+using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class DoctorsController : Controller
+    {
+
+        ApplicationDbcontext dbcontext = new ApplicationDbcontext();
+        public IActionResult BookAppointment()
+        {
+
+            var doctors = new List<Doctor>
+            {
+            new Doctor { Id = 1, Name = "Dr. John Smith", Specialization = "Cardiology", Img = "doctor1.jpg" },
+            new Doctor { Id = 2, Name = "Dr. Sarah Johnson", Specialization = "Pediatrics", Img = "doctor2.jpg" },
+            new Doctor { Id = 3, Name = "Dr. Emily Davis", Specialization = "Dermatology", Img = "doctor4.jpg" },
+             new Doctor { Id = 4, Name = "Dr. Michael Lee", Specialization = "Orthopedics", Img = "doctor3.jpg" },
+            new Doctor { Id = 5, Name = "Dr. William Clark", Specialization = "Neurology", Img = "doctor5.jpg" },
+            };
+            return View(model: doctors);
+        }
+
+        public IActionResult CompleteAppointment(int Id)
+        {
+            var doctors = new List<Doctor>
+    {
+        new Doctor { Id = 1, Name = "Dr. John Smith", Specialization = "Cardiology", Img = "doctor1.jpg" },
+        new Doctor { Id = 2, Name = "Dr. Sarah Johnson", Specialization = "Pediatrics", Img = "doctor2.jpg" },
+        new Doctor { Id = 3, Name = "Dr. Emily Davis", Specialization = "Dermatology", Img = "doctor4.jpg" },
+        new Doctor { Id = 4, Name = "Dr. Michael Lee", Specialization = "Orthopedics", Img = "doctor3.jpg" },
+        new Doctor { Id = 5, Name = "Dr. William Clark", Specialization = "Neurology", Img = "doctor5.jpg" },
+    };
+
+            var doctor = doctors.FirstOrDefault(d => d.Id == Id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+            ViewBag.DoctorName = doctor.Name;
+
+            return View();
+        }
+
+    }
+
+}
---
+++ Hospital/Data/ApplicationDbcontext.cs
@@ -0,0 +1,24 @@
+﻿using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        DbSet<Doctor> doctors {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            
+            base.OnConfiguring(optionsBuilder);
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build()
+                .GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(builder);
+        }
+
+       
+
+    }
+}
